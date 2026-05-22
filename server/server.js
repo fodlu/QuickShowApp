108045a55fa7decf65248254196beac0aa84f1fd
Fadilulahi Opeyemi Musediq <musediqopeyemi@gmail.com>
2026-05-22 04:54:10 -0700

    second commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -16,5 +18,6 @@ app.use(clerkMiddleware())
 
 // API Route
 app.get('/', (req, res) => res.send('Server is live!'));
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, () => console.log(`Server is running on port http://localhost:${port}`));
